455e44de118710e66f0581c86c1da52a22deac17
ChangYeon Hong <hongchangyeon@gmail.com>
2020-07-08 23:36:01 -0500

    initial code for api routing

--- server.js
+++ server.js
@@ -14,21 +14,32 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// tables and waitlist
+// HTML Routing
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
-  
-  app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
-  });
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
- 
-  // Starts the server to begin listening
-  // =============================================================
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-      
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+// API Routing
+
+app.get("/api/notes", function (req, res) {
   
+});
+
+app.post("/api/notes", function (req, res) {
+  
+});
+
+app.get("/api/notes/:id", function (req, res) {
+  
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
